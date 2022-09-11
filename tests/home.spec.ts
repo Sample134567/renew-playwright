f80e098e1b24c7e8c84b1487bfa3433127c782bf
jhayphie Sapitan <pablosapitan28@gmail.com>
2022-09-11 08:30:10 +0800

    Add About page simple testing

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -8,4 +8,12 @@ test.describe('Home', () => {
         // verify title
         await expect(page).toHaveTitle('Practice E-Commerce Site – Automation Bro')
     })
+
+    test('Open aboutpage and verify title', async ({page}) => {
+        // open url
+        await page.goto('https://practice.automationbro.com/about/');
+
+        // verify title
+        await expect(page).toHaveTitle('About – Practice E-Commerce Site')
+    })
 })
