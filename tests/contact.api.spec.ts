7521898b54a877cba276cc077a7f947d2ac0c568
pabs28 <pablosapitan28@gmail.com>
2024-04-02 15:03:58 +0800

    Add API helper

--- tests/contact.api.spec.ts
+++ tests/contact.api.spec.ts
@@ -1,33 +1,34 @@
-import { test, expect, type Page, APIRequestContext } from '@playwright/test';
-import { faker } from '@faker-js/faker';
+import { test, expect, type Page, APIResponse } from '@playwright/test';
 import ContactPage from '../pages/contact.page';
+import apiController from '../controller/api.controller';
 
 
 test.describe('Contact', () => {
-    let contactPage: ContactPage
-    let fakerAPI: APIRequestContext
+    let contactPage: ContactPage;
+    let randomPerson: APIResponse;
 
-    test.beforeAll(async({playwright}) => {
-        fakerAPI = await playwright.request.newContext({
-            baseURL: 'https://jsonplaceholder.typicode.com/'
-        });
+    test.beforeAll(async () => {
 
-        const response = await fakerAPI.get('users');
-        console.log(await response.json())
+        await apiController.init();
+        randomPerson = await apiController.getUsers();
+        const newUserTodo = await apiController.createUserTodo();
+        console.log(newUserTodo);
+
     })
 
-    test('Fill contact form and verify success message', async ({page}) => {
+    test('Fill contact form and verify success message', async ({ page }) => {
         contactPage = new ContactPage(page)
-        
+
         // open contact page
         await contactPage.navigate()
 
         // fill out input fields
-       contactPage.submitForm(faker.person.firstName(), 
-       faker.internet.email(), 
-       faker.phone.number(), 
-       faker.lorem.paragraphs(2))
-       
+        contactPage.submitForm(
+            randomPerson['name'],
+            randomPerson['email'],
+            randomPerson['phone'],
+            randomPerson['website']);
+
         // verify success message
         await expect(contactPage.success).toHaveText('Thanks for contacting us! We will be in touch with you shortly')
     })
